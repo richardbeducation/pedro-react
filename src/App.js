160354e540404912bd52e06bbdfa70dc1c26f4e0
richardbeducation <richardbeducation@gmail.com>
2022-07-13 08:17:34 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import './App.css';
 
+// root component
 function App() {
-  const name = 'Pedro';
   return (
     <div className="App">
-      <h1>Hello {name}</h1>
+      <Jobs salary={90000} position='Senior SDE' company='Amazon'/>
+      <Jobs salary={12000} position='Junior SDE' company='Google'/>
+      <Jobs salary={10000} position='Project Manager' company='Netflix'/>      
     </div>
   );
+}
+
+// A component called Jobs
+const Jobs = (props) => {
+  return (
+    <div>
+      <h1>{props.salary}</h1>
+      <h2>{props.position}</h2>
+      <h3>{props.company}</h3>
+    </div>
+  )
 }
 
 export default App;
